84ece0c9a4b8aee802d54923b0c62e43f868bed7
marcelobojikian <marcelo.bojikian@gmail.com>
2022-07-26 20:41:59 +0100

    Correcao do thema do emulationstation sem sobrescrita de metodo

--- emulationstation/src/main/java/br/com/mnb/theme/emulationstation/xml/theme/EmulationStationTheme.java
+++ emulationstation/src/main/java/br/com/mnb/theme/emulationstation/xml/theme/EmulationStationTheme.java
@@ -1,21 +1,10 @@
 package br.com.mnb.theme.emulationstation.xml.theme;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 
 import br.com.mnb.theme.core.xml.theme.AbstractTheme;
-import br.com.mnb.theme.core.xml.view.View;
 
 @XStreamAlias("theme")
 public class EmulationStationTheme extends AbstractTheme {
-
 	private static final long serialVersionUID = 1L;
-	
-	public EmulationStationTheme addViews(View... elements) {
-		getViewElements().addAll(new ArrayList<>(Arrays.asList(elements)));
-		return this;
-	}
-
 }
