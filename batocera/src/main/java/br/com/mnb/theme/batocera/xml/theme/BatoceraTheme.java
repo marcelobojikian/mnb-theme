79550e5650d6bd2b08f6e4f1765a563e2f043231
marcelobojikian <marcelo.bojikian@gmail.com>
2022-07-27 12:07:37 +0100

    Adicao de metodos que ajudam a inserir novos elementos

--- batocera/src/main/java/br/com/mnb/theme/batocera/xml/theme/BatoceraTheme.java
+++ batocera/src/main/java/br/com/mnb/theme/batocera/xml/theme/BatoceraTheme.java
@@ -1,10 +1,27 @@
 package br.com.mnb.theme.batocera.xml.theme;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 
+import br.com.mnb.theme.core.xml.feature.FeatureElement;
 import br.com.mnb.theme.core.xml.theme.AbstractTheme;
+import br.com.mnb.theme.core.xml.view.View;
 
 @XStreamAlias("theme")
 public class BatoceraTheme extends AbstractTheme {
+	
 	private static final long serialVersionUID = 1L;
+
+	public BatoceraTheme addFeatures(FeatureElement features) {
+		getFeatures().addAll(new ArrayList<>(Arrays.asList(features)));
+		return this;
+	}
+
+	public BatoceraTheme addViews(View... elements) {
+		getViewElements().addAll(new ArrayList<>(Arrays.asList(elements)));
+		return this;
+	}
+	
 }
